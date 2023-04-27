035ac3aff96bb3d644e0e85918058a825fea76e5
Adoniyas7 <adoniyasg7s@gmail.com>
2023-04-28 02:18:01 +0300

    BR-54 added passord reset confirmation email and form

--- cafe/urls.py
+++ cafe/urls.py
@@ -14,7 +14,8 @@ urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("reset_password/", auth_views.PasswordResetView.as_view(), name="rest_password"),
     path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 
 
 ]
