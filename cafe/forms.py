b54154865b69c8f1e6ec366e08aea36b3413f60e
fre527 <fremariame.slomon@bitscollege.edu.et>
2023-05-01 19:22:42 +0300

    BR-55 BR-56 created the user profile editing and view

--- cafe/forms.py
+++ cafe/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 from django import forms
 from .models import Customer, Review
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .validators import validate_email
 
 class CustomerForm(UserCreationForm):
@@ -33,7 +33,7 @@ class CustomerForm(UserCreationForm):
             print('image')
         else:
             print('no image')
-            customer.profile_pic = 'profile_images/default.png'
+            customer.profile_pic = 'profile_pics/default.png'
             customer.save()
         return user
     
@@ -44,3 +44,32 @@ class ReviewForm(forms.ModelForm):
         widgets = {
             'review': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your review'}),
         }
+
+class CustomerProfileForm(UserChangeForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    profile_pic = forms.ImageField(required=False)
+
+    class Meta:
+        model = Customer
+        fields = ('email', 'first_name', 'last_name', 'profile_pic')
+
+    def init(self, *args, **kwargs):
+        super().init(*args, **kwargs)
+        self.fields['email'].initial = self.instance.user.email
+        self.fields['first_name'].initial = self.instance.first_name
+        self.fields['last_name'].initial = self.instance.last_name
+        self.fields['profile_pic'].initial = self.instance.profile_pic
+
+    def save(self, commit=True):
+        customer = super().save(commit=False)
+        customer.user.email = self.cleaned_data['email']
+        customer.first_name = self.cleaned_data['first_name']
+        customer.last_name = self.cleaned_data['last_name']
+        if self.cleaned_data['profile_pic']:
+            customer.profile_pic = self.cleaned_data['profile_pic']
+        if commit:
+            customer.user.save()
+            customer.save()
+        return customer
--- cafe/urls.py
+++ cafe/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
-
+    path("profile/", views.profile, name="profile"),
 
 ]
